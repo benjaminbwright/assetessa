28d1bfd3707820ca0995fec534d13db3daf3d23c
Benjamin Wright <bwright@trilogyed.com>
2020-02-28 17:53:18 -0500

    dynamically generates entries from pagescripts folder in assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../assets/assets.config.js')
+
+const pageScriptDir = config.pageScriptDir || "assets/js/pagescripts";
+
+// setup webpack entry for js from pagescripts
+const jsEntries = {
+  main: path.join(__dirname,"../",pageScriptDir,"main.js")
+}
+
+fs
+  .readdirSync(__dirname + "/../" + pageScriptDir)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file.slice(-3) === ".js");
+  })
+  .forEach(function(file) {
+    jsEntries[file.split('.')[0]] = path.join(__dirname,'../',pageScriptDir,file);
+  });
+  console.log(jsEntries)
+
+// Webpack Config
 module.exports = {
   mode: "development",
-  entry: "./index.js",
+  entry: (Object.keys(jsEntries).length !== 0) ? jsEntries : "./index.js",
   output: {
-    path: __dirname + "/../public/dist",
-    filename: "bundle.js"
+    path: __dirname + "/../dist",
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
