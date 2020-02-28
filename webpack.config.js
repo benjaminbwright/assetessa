33146a5cfb7907b88b09f5802787c65fc49cbd2a
Benjamin Wright <bwright@trilogyed.com>
2020-02-28 17:06:29 -0500

    removed node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,21 @@ module.exports = {
   output: {
     path: __dirname + "/../public/dist",
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
+  
 }
 
