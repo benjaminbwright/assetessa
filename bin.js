fad0510f62dc6b531fb32eb9349dcee00deefb6e
Benjamin Wright <bwright@trilogyed.com>
2020-02-29 04:28:08 -0500

    added webpack functions to bin.js

--- bin.js
+++ bin.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
-const exec = require('child_process').exec
 const run = require('node-run-cmd').run;
 const init = require('./init');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
 const input = process.argv[2];
+
+const compiler =  webpack(webpackConfig);
 
 switch (input) {
   case 'init':
@@ -14,11 +17,22 @@ switch (input) {
   case 'build':
     console.log('Bundling assets...');
     run('npx webpack');
+    compiler.run((err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(stats);
+    });
     break;
   case 'watch':
     console.log('Watching for changes in asset files...')
-    run('npx webpack --watch');
+    compiler.watch({
+      // Example watchOptions
+      aggregateTimeout: 300,
+      poll: undefined
+    }, (err, stats) => { // Stats Object
+      // Print watch/build result here...
+      console.log(stats);
+    })
     break;
 }
-
-
