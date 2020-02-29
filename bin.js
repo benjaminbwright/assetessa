4319ae27f7139de88059b9fefcb6a0e26be49404
Benjamin Wright <bwright@trilogyed.com>
2020-02-29 03:51:39 -0500

    Executeable init, build and watch commands

--- bin.js
+++ bin.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 
+const exec = require('child_process').exec
+const run = require('node-run-cmd').run;
 const init = require('./init');
 
-init();
+const input = process.argv[2];
+
+switch (input) {
+  case 'init':
+    console.log('Initializing Assets...');
+    init();
+    break;
+  case 'build':
+    console.log('Bundling assets...');
+    run('npx webpack');
+    break;
+  case 'watch':
+    console.log('Watching for changes in asset files...')
+    run('npx webpack --watch');
+    break;
+}
+
 
